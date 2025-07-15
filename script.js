5f261467619f5ab8a8b7c038c01d6cefdbd8b45f
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-15 21:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your JS code here. If required.
+let BookForm = document.querySelector(".form-group form");
+
+let bookDetails = {
+  title: "",
+  author: "",
+  isbn: "",
+  delete: "",
+};
+
+BookForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  bookDetails.title = document.getElementById("title").value.trim();
+  bookDetails.author = document.getElementById("author").value.trim();
+  bookDetails.isbn = document.getElementById("isbn").value.trim();
+
+  console.log(bookDetails);
+
+  let tbody = document.getElementById("book-list");
+
+  let newRow = document.createElement("tr");
+
+  // create and populate cells
+  let titleCell = document.createElement("td");
+  titleCell.textContent = bookDetails.title;
+
+  let authorCell = document.createElement("td");
+  authorCell.textContent = bookDetails.author;
+
+  let isbnCell = document.createElement("td");
+  isbnCell.textContent = bookDetails.isbn;
+
+  let deleteCell = document.createElement("td");
+  deleteCell.textContent = "❌";
+
+  deleteCell.classList.add("delete");
+  deleteCell.style.cursor = "pointer";
+
+  deleteCell.addEventListener("click", function () {
+    newRow.remove();
+  });
+
+  newRow.append(titleCell, authorCell, isbnCell, deleteCell);
+
+  tbody.appendChild(newRow);
+});
